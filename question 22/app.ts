0835e0df3c11b844d2982ffa157b3904edbae4fc
SobiaMunir <sobimunir1@gmail.com>
2024-07-09 19:22:32 +0500

    Add files via upload

---
+++ question 22/app.ts
@@ -0,0 +1,7 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+//index error
+let listing: string[] = [`iqra`, `hafsa`, `safa`, `amaan`];
+console.log(listing[4]);
+//correcting error
+let listings: string[] = [`iqra`, `hafsa`, `safa`, `amaan`];
+console.log(listings[3]);
