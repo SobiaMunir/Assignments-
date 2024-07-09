68abff995cabf2a48fc5043b9d5a19691fdd7edd
SobiaMunir <sobimunir1@gmail.com>
2024-07-09 19:14:37 +0500

    Add files via upload

---
+++ question 19/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+var app_1 = require("../Q14/app");
+console.log("\n number of guests");
+console.log("we have successfully imported ".concat(app_1.guest_list, " from Ex 14."));
---
+++ question 19/app.ts
@@ -0,0 +1,4 @@
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+import { guest_list } from "../Q14/app";
+console.log("\n number of guests");
+console.log(`we have successfully imported ${guest_list} from Ex 14.`);
