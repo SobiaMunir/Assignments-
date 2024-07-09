cc4b11c88b02a469153169b06a66989559d3fe38
SobiaMunir <sobimunir1@gmail.com>
2024-07-09 21:40:53 +0500

    Add files via upload

---
+++ question 9/main.js
@@ -0,0 +1,4 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var favoriteNumber = 7;
+var message = "My favouritr number:".concat(favoriteNumber);
+console.log(message);
---
+++ question 9/main.ts
@@ -0,0 +1,5 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoriteNumber: number = 7;
+let message: string = `My favouritr number:${favoriteNumber}`;
+console.log(message);
