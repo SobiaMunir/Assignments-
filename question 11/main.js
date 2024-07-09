c08f6d72b5df49bd84296452d5a06831c26a388d
SobiaMunir <sobimunir1@gmail.com>
2024-07-09 21:35:40 +0500

    Add files via upload

---
+++ question 11/main.js
@@ -0,0 +1,7 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+//define an array of names
+var names = ["sana", "sobia", "hamna", "hadiya", "hina",];
+//for loop method
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
