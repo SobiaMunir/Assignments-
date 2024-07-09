288ab4201afdd22d164fd5a20d2df699fab99dd0
SobiaMunir <sobimunir1@gmail.com>
2024-07-09 21:17:43 +0500

    Add files via upload

---
+++ question 3/app.js
@@ -0,0 +1,8 @@
+// Q NO 2 NAME CASE
+var firstName = "sobia ";
+//to upper case
+console.log(firstName.toUpperCase());
+//to lower case
+console.log(firstName.toLowerCase());
+//title case
+console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase());
