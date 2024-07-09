880947fbfcea45971ddece9e8577d0d04f14291b
SobiaMunir <sobimunir1@gmail.com>
2024-07-09 21:38:32 +0500

    Add files via upload

---
+++ question 10/main.js
@@ -0,0 +1,7 @@
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// this  program add two number and print result
+var number1 = 10;
+var number2 = 50;
+//add two numbers
+var result = number1 + number2;
+console.log(result);
