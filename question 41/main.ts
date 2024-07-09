cb0e600905d3f71f93f767500cf3bfa9d18382dd
SobiaMunir <sobimunir1@gmail.com>
2024-07-09 20:44:28 +0500

    Add files via upload

---
+++ question 41/main.ts
@@ -0,0 +1,18 @@
+//Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+
+function show_magicians(magicians: string[]): void {
+  for (let magician of magicians) {
+    console.log(magician);
+  }
+}
+
+// Array of magician's names
+let magicians: string[] = [
+  "Harry Houdini",
+  "David Copperfield",
+  "Penn Jillette",
+  "Teller",
+];
+
+// Call the function to show the magician's names
+show_magicians(magicians);
